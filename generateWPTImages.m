5f6710a3a16ce53a793ada4ee2773184daa5dabd
leksea <leksea@gmail.com>
2014-06-13 18:28:08 -0700

    PS-scrambling function added

--- generateWPTImages.m
+++ generateWPTImages.m
@@ -195,3 +195,63 @@ end
         end;
         out = median(mag,3);
     end
+    
+    %% Portia - Simoncelli scrambling tool
+    % imSet{group}{image} -- cell matrix of raw (unfiltered, uncut) wallpapers
+    % out{group}{image} -- scrambled wallpaper in frequency domain  
+    function [scrambled, freqScrambled] = psScramble(imSet)
+
+        Nsc = 4; % Number of scales
+        Nor = 4; % Number of orientations
+        Na = 11;  % Spatial neighborhood is Na x Na coefficients It must be an odd number!
+
+        Niter = 15;	% Number of iterations of synthesis loop
+    
+
+        x = numel(imSet);
+        scrambled = cell(x, 1);
+        freqScrambled = cell(x, 1);
+        for i = 1:x
+            
+            y = numel(imSet{i});
+            % two cell arrays to store freq-scrambled and raw-scrambled
+            scrambled{i} = cell(y, 1);
+            freqScrambled{i} = cell(y, 1);
+            
+            for j = 1:y
+                
+                image = imSet{i}{j};
+                Nx = size(image, 1);
+                Ny = size(image, 2);
+                
+                %for scrambling algorithm, image dimentions have to be  multiple of 2^(Nsc+2)
+                req_mult = 2^(Nsc + 2);
+                
+                req_Nx = req_mult*floor(Nx/req_mult);
+                req_Ny = req_mult*floor(Ny/req_mult);
+                % if current wallpaper's size is less than required 
+                
+                if (req_Nx > Nx || req_Ny > Ny)
+                   added_X = cat(2, image, image(:, 1:req_mult));
+                   added_XY = cat(1, added_X, added_X(1:req_mult, :));
+                   image = added_XY;
+                end
+                
+                Nsx = req_Nx;	
+                Nsy = req_Ny;
+                mask = zeros(Nsx, Nsy);
+                imKeep = zeros(Nsx*Nsy, 1);
+                im0 = image(1:Nsx, 1:Nsy);
+            
+                params = textureAnalysis(im0, Nsc, Nor, Na);
+
+                imKeep(:, 1) = reshape(mask, [Nsx*Nsy, 1]);
+                imKeep(:, 2) = reshape(im0, [Nsx*Nsy, 1]);
+
+                res = textureSynthesis(params, [Nsx Nsy], Niter, [], imKeep);
+                scrambled{i}{j} = res;
+                freqScrambled{i}{j} = fft2(res);
+            end
+        end
+    end    
+
