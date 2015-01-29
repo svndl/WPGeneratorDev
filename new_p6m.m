fa668e4da06c7b6b2d8fac207c02613f35ea900c
Alexandra Yakovleva <leksea@gmail.com>
2015-01-28 17:35:46 -0800

    Update new_p6m.m
    Replaced clipping with rotation

--- new_p6m.m
+++ new_p6m.m
@@ -60,7 +60,9 @@ function p6m = new_p6m(tile)
     tile2 = max(tri, shifted);
     t2 = floor(0.5*size(tile2, 1));
     
-    tile2_flipped = [tile2(t2 + 1:end, :); tile2(1:t2, :)];
+    %tile2_flipped = [tile2(t2 + 1:end, :); tile2(1:t2, :)];
+    tile2_flipped = rot90(tile2, 2); 
+    
     %size(tile3) = [2y1 x 6x1]
     tile3 = [tile2, tile2_flipped];
     p6m = imresize(tile3, 1/magfactor, 'bicubic');
